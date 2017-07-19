19f73dd5c1d35ea7552cddf071c2b2222aab64d3
Zhhyman <Zhyman13@gmail.com>
2017-07-19 10:43:28 -0400

    Added img and title tag

--- src/App.js
+++ src/App.js
@@ -26,7 +26,13 @@ export default class Post extends Component {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices, elit in laoreet suscipit, tortor lectus aliquam leo, non lacinia turpis orci vel ligula. Donec mi elit, bibendum vel arcu vulputate, posuere ultrices ipsum. Suspendisse aliquet elementum nunc ut ornare. Quisque et rutrum purus. Cras eget lectus ac ex ullamcorper tempor nec a turpis. In hac habitasse platea dictumst.
           </div>
           <div className="Main">
-            
+            <img className="Main_Pic"
+              src="https://scontent.xx.fbcdn.net/v/t1.0-9/14449792_10157433035660048_5027347782728027455_n.jpg?oh=e6f4009cfb3196bc195e043ff44c7bdc&oe=5A0FD645"
+              alt="Content's main"
+            />
+            <div className="Main-Title">
+              <h3>Your title here</h3>
+            </div>
           </div>
         </div>
         <div className="Reaction-Bar">
